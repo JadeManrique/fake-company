61576c9de15812cc39e6aa760a6a2b84314f7b14
Jade Manrique <jade.a.manrique@gmail.com>
2018-03-18 13:04:41 -0700

    Finished basic webpage structure

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,1 +1,28 @@
-console.log("hello");
+$(document).ready( function () {
+
+
+
+  $("#article div img").click( function () {
+
+    var imageLink = $(this).attr("src");
+
+    var imageText = "<img src=\'" + imageLink + "\' />";
+
+    $("#lightbox").html(imageText);
+
+    });
+
+
+    $("#article .imgBar img").hover( function(e) {
+
+          $(this).animate({opacity: 1.0, height: "auto", width: "105px"}, 150);
+
+      }, function() {
+
+          $(this).animate({opacity: 0.5, height: "auto", width: "100px"}, 150);
+
+      });
+
+
+
+  });
